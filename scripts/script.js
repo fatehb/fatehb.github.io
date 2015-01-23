c63bf6682bc0735149fc8ab3f8dfeddaa33214e8
Muhammad Bajwa <fatehb91@gmail.com>
2015-01-22 21:47:06 -0500

    update, adding JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-js page
+$(document).ready(function(e) {
+	$("div#topCont").slideUp(200).delay(200).slideDown(1000);
+    $("div#midCont").hide(500).delay(200).show(1000); 
+	$("footer").slideUp(700).delay(200).slideDown(1000);
+});
